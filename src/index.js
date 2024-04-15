8de967a2685f651c0ecbc1f081cc189af6661948
ckhepburn <162160743+ckhepburn@users.noreply.github.com>
2024-04-15 13:56:33 -0400

    Updated HTML into CSS and created function

--- src/index.js
+++ src/index.js
@@ -53,7 +53,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ['Tue', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  let forecastHtml = '';
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day"> 
+   <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+        <strong>15℃</strong>
+        </div>
+    <div class="weather-forecast-temperature">9℃</div>
+</div>
+</div>
+`;
+  });
+
+  let forecastElement = document.querySelector('#forecast');
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Paris');
+displayForecast();
